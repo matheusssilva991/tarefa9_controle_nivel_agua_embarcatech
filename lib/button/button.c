16f80597e84132619a479efac2e05d840dc9eff6
Arthur Moreira <arthur_moreira.10@hotmail.com>
2025-06-18 20:31:12 -0300

    Adiciona bilioteca dos botões

--- lib/button/button.c
+++ lib/button/button.c
@@ -1,19 +1,51 @@
 #include "button.h"
 
-void init_btn(uint8_t pin)
-{
+volatile uint32_t last_debounce_time = 0;
+const uint8_t debounce_delay = 250;
+
+void button_init(uint8_t pin){
     gpio_init(pin);
-    gpio_set_dir(pin, GPIO_IN);
+    gpio_set_dir(pin,GPIO_IN);
     gpio_pull_up(pin);
 }
 
-void init_btns()
-{
-    init_btn(BTN_A_PIN);
-    init_btn(BTN_B_PIN);
+void button_init_predefined(bool A,bool B,bool SW){
+
+    if(A){
+        button_init(BUTTON_A);
+    }
+    if (B)
+    {
+        button_init(BUTTON_B);
+    }
+    if (SW)
+    {
+        button_init(BUTTON_SW);
+    }
+
 }
 
-bool btn_is_pressed(uint8_t pin)
-{
-    return !gpio_get(pin); // Retorna verdadeiro se o botão estiver pressionado
+void button_setup_irq(bool A,bool B,bool SW,gpio_irq_callback_t callback){
+
+    gpio_set_irq_callback(callback);
+
+    if (A && gpio_get(BUTTON_A))
+    {
+        gpio_set_irq_enabled(BUTTON_A, GPIO_IRQ_EDGE_FALL, true);
+    }
+    if (B && gpio_get(BUTTON_B))
+    {
+        gpio_set_irq_enabled(BUTTON_B, GPIO_IRQ_EDGE_FALL, true);
+    }
+    if (SW && gpio_get(BUTTON_SW))
+    {
+        gpio_set_irq_enabled(BUTTON_SW, GPIO_IRQ_EDGE_FALL, true);
+    }
+    
+}
+
+bool button_is_pressed(uint8_t pin){
+
+    return !gpio_get(pin);  // true se pressionado (nível baixo)
+
 }
--- lib/button/button.h
+++ lib/button/button.h
@@ -1,15 +1,27 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
-#include <stdlib.h>
 #include "pico/stdlib.h"
 
-#define BTN_A_PIN 5 // GPIO para botão A
-#define BTN_B_PIN 6 // GPIO para botão B
-#define BTN_SW_PIN 22 // GPIO para botão do joystick
+// Pinos padrão para os botões
+#define BUTTON_A 5
+#define BUTTON_B 6
+#define BUTTON_SW 22
 
-void init_btn(uint8_t pin);
-void init_btns();
-bool btn_is_pressed(uint8_t pin);
+extern volatile uint32_t last_debounce_time;
+extern const uint8_t debounce_delay;
+
+// Inicializa um botão em qualquer pino
+void button_init(uint8_t pin);
+
+// Inicializa os botões padrão A, B e SW com pull-up interno
+void button_init_predefined(bool A, bool B, bool SW);
+
+// Configura interrupções para os botões A, B e SW
+void button_setup_irq(bool A, bool B, bool SW, gpio_irq_callback_t callback);
+
+// Retorna true se o botão estiver pressionado (nível baixo)
+bool button_is_pressed(uint8_t pin);
+
 
 #endif // BUTTON_H
